35e06e695d5b99e4f380eaaee8ff7b95405a044d
lusionx <lusionx@d0cd7490-2d8a-11df-b28a-19025b8c0740>
2010-12-12 11:52:47 +0000

    他妈的, gae下的 urllib2 的代理是不能用的

--- python/lusionx/proxy.py
+++ python/lusionx/proxy.py
@@ -1,10 +1,13 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
+# Filename: proxy.py
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.ext.webapp import template
 import kit
 from models import Proxy
+from google.appengine.api import urlfetch
 
 def getform(entity,request):
     entity.ip = request.get('ip')
@@ -22,7 +25,11 @@ view_data = {
 
 class list(webapp.RequestHandler):
     def get(self):
-        pass
+        q = Proxy.all()
+        view_data['title'] = '代理 列表'
+        view_data['entities'] = q
+        html = kit.render_view(view_data,'list','proxy')
+        self.response.out.write(html)
 kit.add_act(r'/proxy/list',list)
 
 class edit(webapp.RequestHandler):
@@ -32,12 +39,42 @@ class edit(webapp.RequestHandler):
         pass
 kit.add_act(r'/proxy/edit/(\d+)',edit)
 
+import urllib2
+class fetch(webapp.RequestHandler):
+    def get(self,id=0):
+        one = Proxy.get_by_id(int(id))
+        url = 'http://www.geoiptool.com/'
+        add = 'http://wsdwss'+one.ip+':'+str(one.port)
+        proxy_support = urllib2.ProxyHandler({
+            'http': add
+            })
+        opener = urllib2.build_opener(proxy_support, urllib2.HTTPHandler)
+        urllib2.install_opener(opener)
+        msg = ''
+        try:
+            result = urllib2.urlopen(url)
+            msg = result.read()
+        except urllib2.URLError, e:
+            msg = '抓取出错'
+        #我的ip 125.64.124.215
+        self.response.out.write(msg+add)
+kit.add_act(r'/proxy/fetch/(\d+)',fetch)
+
 class new(webapp.RequestHandler):
     def get(self):
-        pass
+        one = Proxy()
+        view_data['title'] = '添加一个代理'
+        view_data['act'] = '/proxy/new'
+        view_data['entity'] = one
+        html = kit.render_view(view_data,'form','proxy')
+        self.response.out.write(html)
     def post(self):
-        pass
-kit.add_act(r'/proxy/edit/(\d+)',edit)
+        one = Proxy()
+        getform(one,self.request)
+        one.put()
+        self.redirect('/proxy/list')
+
+kit.add_act(r'/proxy/new',new)
 
 kit.add_act(r'/proxy.*',list)
 
