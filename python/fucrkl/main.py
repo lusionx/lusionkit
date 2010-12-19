b8d194b9e88ce2a5dc3804359ef18b569a7db6ed
lusionx <lusionx@d0cd7490-2d8a-11df-b28a-19025b8c0740>
2010-12-19 03:22:02 +0000

    通过 压缩包的方式 使用最新的django框架

--- python/fucrkl/main.py
+++ python/fucrkl/main.py
@@ -2,42 +2,38 @@
 # -*- coding: utf-8 -*-
 # Filename: main.py
 #
-import logging, os
+import sys, os, logging
 
-# Google App Engine imports.
-from google.appengine.ext.webapp import util
+# Uninstall Django 0.96.
+for k in [k for k in sys.modules if k.startswith('django')]:
+    del sys.modules[k]
 
+# Add Django 1.0 archive to the path.
+django_path = os.path.join(os.path.dirname(__file__),'django.zip')
+#django_path = 'django.zip'
+sys.path.insert(0, django_path)
+
+
+# Must set this env var before importing any part of Django
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 # Force Django to reload its settings.
 from django.conf import settings
 settings._target = None
 
-# Must set this env var before importing any part of Django
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-
-import logging
-import django.core.handlers.wsgi
-import django.core.signals
-import django.db
-import django.dispatch.dispatcher
 
 def log_exception(*args, **kwds):
-  logging.exception('Exception in request:')
+    logging.exception('Exception in request:')
 
-# Log errors.
-django.dispatch.dispatcher.connect(
-   log_exception, django.core.signals.got_request_exception)
 
-# Unregister the rollback event handler.
-django.dispatch.dispatcher.disconnect(
-    django.db._rollback_on_exception,
-    django.core.signals.got_request_exception)
+from django.core.handlers import wsgi
+from google.appengine.ext.webapp import util
 
 def main():
   # Create a Django application for WSGI.
-  application = django.core.handlers.wsgi.WSGIHandler()
+    application = wsgi.WSGIHandler()
 
   # Run the WSGI CGI handler with that application.
-  util.run_wsgi_app(application)
+    util.run_wsgi_app(application)
 
 if __name__ == '__main__':
-  main()
+    main()
